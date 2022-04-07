ac636871102215dc5025de9579567851384f89b4
tamam03 <tamam0308@gmail.com>
2022-04-07 06:52:57 +0000

    favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,10 +2,8 @@ class FavoritesController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
-    # favorite = current_user.favorites.new(book_id: book.id)
     favorite = Favorite.new(book_id: book.id, user_id: current_user.id)
     favorite.save
-    redirect_to request.referer
 
   end
 
@@ -13,7 +11,6 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to request.referer
 
   end
 end
