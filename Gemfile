19b4b336e31ff3c240b6a813e6723fe38f2ef330
tamam03 <tamam0308@gmail.com>
2022-04-07 07:47:53 +0000

    Revert "gemfile"
    This reverts commit 321f68dbd9893770e8739cc25b06e199f2495f61.

--- Gemfile
+++ Gemfile
@@ -56,5 +56,3 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 
 gem 'image_processing'
-
-gem 'jquery-rails'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-// import Rails from "@rails/ujs"
+import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-// import "jquery"
+import "jquery"
 import "popper.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
-import "../stylesheets/application"
+import "../stylesheets/application" 
 
-// Rails.start()
+Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
