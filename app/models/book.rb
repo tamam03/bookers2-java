191fc4303a8bfaca8a44cafe007c8422ed71beba
tamam03 <tamam0308@gmail.com>
2022-04-07 06:20:05 +0000

    first commit

---
+++ app/models/book.rb
@@ -0,0 +1,36 @@
+class Book < ApplicationRecord
+  
+  has_one_attached :profile_image
+  
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
+
+  
+  
+  
+  belongs_to :user
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+
+ 
+
+  
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+  
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @book = Book.where("title LIKE?","#{word}")
+    elsif search == "forward_match"
+      @book = Book.where("title LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @book = Book.where("title LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @book = Book.where("title LIKE?","%#{word}%")
+    else
+      @book = Book.all
+    end
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,70 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+  has_one_attached :profile_image   
+  
+  
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+
+  
+  
+
+  has_many :books, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+
+ 
+
+  
+
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+     profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+
+  def following?(user)
+    followings.include?(user)
+  end
+  
+   def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+   end
+
+end
+
+
